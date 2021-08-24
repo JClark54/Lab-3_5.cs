371624ba679242e4bda749e908caf3db5aa8a8a4
JClark54 <85198081+JClark54@users.noreply.github.com>
2021-08-23 21:14:27 -0500

    Add files via upload

---
+++ 3_5 LAB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3_5_LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "Viagra";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord));
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+
+            }
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
